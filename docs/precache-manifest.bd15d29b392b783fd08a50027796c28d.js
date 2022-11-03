a52bd5090f9e3480aeb59906f33b735f4d6d7995
tourtiere <tourtiere@users.noreply.github.com>
2022-11-03 15:06:12 -0400

    add docs

---
+++ docs/precache-manifest.bd15d29b392b783fd08a50027796c28d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a731a6da7ce6415096aa",
+    "url": "./static/css/main.2008035e.chunk.css"
+  },
+  {
+    "revision": "a731a6da7ce6415096aa",
+    "url": "./static/js/main.a731a6da.chunk.js"
+  },
+  {
+    "revision": "2ab6c3e1b158ef470c44",
+    "url": "./static/js/1.2ab6c3e1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ae990e80be9a9904db60b0d3d06adbc1",
+    "url": "./static/media/fa-brands-400.ae990e80.woff2"
+  },
+  {
+    "revision": "d6ff69c82cc37b323ceb5ff20224cc99",
+    "url": "./static/media/fa-brands-400.d6ff69c8.woff"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "./static/media/fa-brands-400.c6aef942.ttf"
+  },
+  {
+    "revision": "1010b72339fe3292aede3543e827b741",
+    "url": "./static/media/fa-brands-400.1010b723.eot"
+  },
+  {
+    "revision": "9c0eb6c2e967eccd837da618bcbde91c",
+    "url": "./static/media/fa-regular-400.9c0eb6c2.woff2"
+  },
+  {
+    "revision": "87f37798dcdf11a68de16eacfd566aa3",
+    "url": "./static/media/fa-regular-400.87f37798.woff"
+  },
+  {
+    "revision": "065edea411671c8caceecb999354cc21",
+    "url": "./static/media/fa-regular-400.065edea4.eot"
+  },
+  {
+    "revision": "5dc39cf0aed41e1a5e9918bea7e8603d",
+    "url": "./static/media/fa-regular-400.5dc39cf0.svg"
+  },
+  {
+    "revision": "a20e6745adc3184de8330b1f0579161f",
+    "url": "./static/media/fa-solid-900.a20e6745.eot"
+  },
+  {
+    "revision": "52134b924fd61958f88323845deffc64",
+    "url": "./static/media/fa-solid-900.52134b92.woff2"
+  },
+  {
+    "revision": "a6de637ca793ed8092597012d691a6a0",
+    "url": "./static/media/fa-solid-900.a6de637c.woff"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "./static/media/fa-solid-900.872545dd.ttf"
+  },
+  {
+    "revision": "2e562d4ebf15395f00bc738738f79291",
+    "url": "./static/media/fa-regular-400.2e562d4e.ttf"
+  },
+  {
+    "revision": "c95f5f7a8923b8bb5034de9eb48901d7",
+    "url": "./static/media/fa-brands-400.c95f5f7a.svg"
+  },
+  {
+    "revision": "56404b18f3d1a3eaeed35b43343cfab9",
+    "url": "./static/media/fa-solid-900.56404b18.svg"
+  },
+  {
+    "revision": "5daf508e3fa4ec2289d5477a9aff8dca",
+    "url": "./index.html"
+  }
+];
